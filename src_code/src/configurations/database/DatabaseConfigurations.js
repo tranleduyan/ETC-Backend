8ac949ce035b6d8c30f82dbf355c6907a8c0746f
leduyantran <trana19@spu.edu>
2023-11-03 02:16:55 -0700

    configure database

--- src_code/src/configurations/database/DatabaseConfigurations.js
+++ src_code/src/configurations/database/DatabaseConfigurations.js
@@ -2,16 +2,27 @@
 
 /** Initialize Knex */
 const knex = require("knex");
+require('dotenv').config();
 
 const config = {
   client: "mysql",
   connection: {
-    host: "",
-    user: "",
-    password: "",
-    database: "",
+    host: process.env.DB_HOST_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DEV_NAME,
   },
-}; // Config for deploy
+}; // Config for dev
+
+// const config = {
+//   client: "mysql",
+//   connection: {
+//     host: process.env.DB_HOST_NAME,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_PROD_NAME,
+//   },
+// }; //Config for deploy
 
 const db = knex(config);
 
