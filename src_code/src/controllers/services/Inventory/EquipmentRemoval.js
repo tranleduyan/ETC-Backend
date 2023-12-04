3b0492ac355ce32a3efae9d64dfa454ecb6da67a
Aman Siid <amman.siid@gmail.com>
2023-12-03 19:08:06 -0800

    Files and necessary implementations

---
+++ src_code/src/controllers/services/Inventory/EquipmentRemoval.js
@@ -0,0 +1,27 @@
+const responseBuilder = require("../../../utils/interfaces/IResponseBuilder");
+const db = require("../../../configurations/database/DatabaseConfigurations");
+const dbHelper = require("../../../utils/interfaces/IDBHelperFunctions");
+
+/**
+ * Handle inventory Eqipment Removal by adminstrator.
+ *
+ * @param {object} res - The response object for the HTTP request.
+ * @param {object} req - The request object from the HTTP request.
+ * @returns {object} - This response object indicates the result of the update attempt.
+ * 
+ * Expected Request Body: 
+ * {
+ *      "Equipment": "Type"
+ * }
+ * 
+ * Response is the message with status code 200 if successfully
+ * Else return a server error of status code 503 (see ResponsiveBuilder.js) - the error are trying to input invalid format to database or any thing else that cannot be seen forward
+ */
+async function EqipmentRemoval(res, req) {
+    return null;
+}
+
+/** Exports the module/functions */
+module.exports = {
+    EqipmentRemoval
+}
---
+++ src_code/src/controllers/services/Inventory/TypeAddition.js
@@ -0,0 +1,27 @@
+const responseBuilder = require("../../../utils/interfaces/IResponseBuilder");
+const db = require("../../../configurations/database/DatabaseConfigurations");
+const dbHelper = require("../../../utils/interfaces/IDBHelperFunctions");
+
+/**
+ * Handle inventory Type Addition by adminstrator.
+ *
+ * @param {object} res - The response object for the HTTP request.
+ * @param {object} req - The request object from the HTTP request.
+ * @returns {object} - This response object indicates the result of the update attempt.
+ * 
+ * Expected Request Body: 
+ * {
+ *      "Type": "Type"
+ * }
+ * 
+ * Response is the message with status code 200 if successfully
+ * Else return a server error of status code 503 (see ResponsiveBuilder.js) - the error are trying to input invalid format to database or any thing else that cannot be seen forward
+ */
+async function TypeAddition(res, req) {
+    return null;
+}
+
+/** Exports the module/functions */
+module.exports = {
+    TypeAddition
+}
