2f29b83bd7f5e360f8d62d5a38f9c7d30bc1adf3
Le Duy An Tran <114903308+tranleduyan@users.noreply.github.com>
2024-05-03 00:19:22 -0700

    Update IInventory.js

--- src_code/src/controllers/interfaces/IInventory.js
+++ src_code/src/controllers/interfaces/IInventory.js
@@ -1,11 +1,11 @@
 /** Initialize neccessary modules */
-const EquipmentAdditionService = require('../services/Inventory/EquipmentAddition');
-const UpdateEquipmentService = require('../services/Inventory/EquipmentUpdate');
-const EquipmentRemovalService = require('../services/Inventory/EquipmentRemoval');
-const ExportCSVService = require('../services/Inventory/ExportCSV');
-const ModelAdditionService = require('../services/Inventory/ModelAddition');
-const TypeAdditionService = require('../services/Inventory/TypeAddition');
-const GetAllTypesService = require('../services/Inventory/GetAllTypes');
+const EquipmentAdditionService = require("../services/Inventory/EquipmentAddition");
+const UpdateEquipmentService = require("../services/Inventory/EquipmentUpdate");
+const EquipmentRemovalService = require("../services/Inventory/EquipmentRemoval");
+const ExportCSVService = require("../services/Inventory/ExportCSV");
+const ModelAdditionService = require("../services/Inventory/ModelAddition");
+const TypeAdditionService = require("../services/Inventory/TypeAddition");
+const GetAllTypesService = require("../services/Inventory/GetAllTypes");
 const GetAllModelsOfTypeService = require("../services/Inventory/GetAllModelsOfType");
 const ModelsRemovalServices = require("../services/Inventory/ModelsRemoval");
 const GetTypeInformationService = require("../services/Inventory/GetTypeInformation");
@@ -21,29 +21,32 @@ const ScanAdditionService = require("../services/Inventory/AntennaScan");
 const AntennaAdditionService = require("../services/Inventory/AddRFIDAntenna");
 const AntennaRemovalService = require("../services/Inventory/AntennaRemoval");
 const AntennaUpdateService = require("../services/Inventory/AntennaUpdate");
+const GetAllAntennasService = require("../services/Inventory/GetAllAntennas");
 
 /** Exports the module */
 module.exports = {
-    ExportCSV: ExportCSVService.ExportCSV,
-    EquipmentAddition: EquipmentAdditionService.EquipmentAddition,
-    EquipmentUpdate: UpdateEquipmentService.EquipmentUpdate,
-    EquipmentRemoval: EquipmentRemovalService.EquipmentRemoval,
-    ModelAddition: ModelAdditionService.ModelAddition,
-    TypeAddition: TypeAdditionService.TypeAddition,
-    GetAllTypes: GetAllTypesService.GetAllTypes,
-    GetAllModelsOfType: GetAllModelsOfTypeService.GetAllModelsOfType,
-    ModelsRemoval: ModelsRemovalServices.ModelsRemoval,
-    GetTypeInformation: GetTypeInformationService.GetTypeInformation,
-    GetModelInformation: GetModelInformationService.GetModelInformation,
-    TypesRemoval: TypesRemovalServices.TypesRemoval,
-    TypeUpdate: TypeUpdateServices.TypeUpdate, 
-    ModelUpdate: ModelUpdateServices.ModelUpdate,
-    GetAllModels: GetAllModelsServices.GetAllModels,
-    GetAvailableModels: GetAvailableModelsServices.GetAvailableModels,
-    GetAllEquipment: GetAllEquipmentServices.GetAllEquipment,
-    GetEquipmentInformation: GetEquipmentInformationServices.GetEquipmentInformation,
-    AntennaScan: ScanAdditionService.AntennaScan,
-    AddRFIDAntenna: AntennaAdditionService.AddRFIDAntenna,
-    AntennaRemoval: AntennaRemovalService.AntennaRemoval,
-    AntennaUpdate: AntennaUpdateService.AntennaUpdate
-}
+  ExportCSV: ExportCSVService.ExportCSV,
+  EquipmentAddition: EquipmentAdditionService.EquipmentAddition,
+  EquipmentUpdate: UpdateEquipmentService.EquipmentUpdate,
+  EquipmentRemoval: EquipmentRemovalService.EquipmentRemoval,
+  ModelAddition: ModelAdditionService.ModelAddition,
+  TypeAddition: TypeAdditionService.TypeAddition,
+  GetAllTypes: GetAllTypesService.GetAllTypes,
+  GetAllModelsOfType: GetAllModelsOfTypeService.GetAllModelsOfType,
+  ModelsRemoval: ModelsRemovalServices.ModelsRemoval,
+  GetTypeInformation: GetTypeInformationService.GetTypeInformation,
+  GetModelInformation: GetModelInformationService.GetModelInformation,
+  TypesRemoval: TypesRemovalServices.TypesRemoval,
+  TypeUpdate: TypeUpdateServices.TypeUpdate,
+  ModelUpdate: ModelUpdateServices.ModelUpdate,
+  GetAllModels: GetAllModelsServices.GetAllModels,
+  GetAvailableModels: GetAvailableModelsServices.GetAvailableModels,
+  GetAllEquipment: GetAllEquipmentServices.GetAllEquipment,
+  GetEquipmentInformation:
+    GetEquipmentInformationServices.GetEquipmentInformation,
+  AntennaScan: ScanAdditionService.AntennaScan,
+  AddRFIDAntenna: AntennaAdditionService.AddRFIDAntenna,
+  AntennaRemoval: AntennaRemovalService.AntennaRemoval,
+  AntennaUpdate: AntennaUpdateService.AntennaUpdate,
+  GetAllAntennas: GetAllAntennasService.GetAllAntennas,
+};
