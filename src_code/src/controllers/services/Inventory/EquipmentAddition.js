33eeda13cb98ea6be8d2910d015de0d5a9e2e941
leduyantran <114903308+tranleduyan@users.noreply.github.com>
2024-05-20 21:37:19 -0700

    Only insert if homeLocations is not empty.

--- src_code/src/controllers/services/Inventory/EquipmentAddition.js
+++ src_code/src/controllers/services/Inventory/EquipmentAddition.js
@@ -79,11 +79,12 @@ async function EquipmentAddition(res, req) {
             FK_EQUIPMENT_SERIAL_ID: serialId.trim()
         }))
 
-        await trx("equipment_home").insert(newHomeLocationData)
+        if(homeLocations.length > 0){
+            await trx("equipment_home").insert(newHomeLocationData)
 
-        /** Commit the transaction */
-        await trx.commit();
-
+            /** Commit the transaction */
+            await trx.commit();
+        }
         /** Return a success response */
         return responseBuilder.BuildResponse(res, 200, {
             message: "New equipment added successfully.",
